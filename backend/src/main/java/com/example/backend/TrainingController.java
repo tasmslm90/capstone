4e7c2957900b0fa67d942a6cf9bbdf2123eb59ad
tasmuslum90 <tasmslm1990@gmail.com>
2023-07-26 22:47:27 +0200

    add getendpoint backend and frontend

--- backend/src/main/java/com/example/backend/TrainingController.java
+++ backend/src/main/java/com/example/backend/TrainingController.java
@@ -17,25 +17,6 @@ public class TrainingController {
     public List<Training> getAllTraining(){
         return this.trainingService.getAllTraining();
     }
-    @PostMapping
-    public Training addTraining(@RequestBody DtoTraining dtoTraining ){
-        Training training = trainingService.addTraining(dtoTraining);
-        return training;
-    }
-    @DeleteMapping("/{id}")
-    public void deleteAnimal(@PathVariable String id) {
-        trainingService.deleteTraining(id);
-    }
-
-    @PutMapping("{id}")
-    public ResponseEntity<Training> editTraining(@RequestBody DtoTraining dtoTraining, @PathVariable String id){
-        try{
-            Training editedTraining = trainingService.editTraining(dtoTraining,id);
-            return ResponseEntity.ok(editedTraining);
-        }catch (Exception e){
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
-    }
 
 }
 
--- backend/src/main/java/com/example/backend/TrainingService.java
+++ backend/src/main/java/com/example/backend/TrainingService.java
@@ -10,22 +10,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TrainingService {
     private final TrainingRepository trainingRepository;
-    private final UuidService uuidService;
 
     public List<Training> getAllTraining(){return this.trainingRepository.findAll();}
-    public Training addTraining(DtoTraining dtoTraining){
-        String id = uuidService.generateUUID();
-        Training training = new Training();
-        training.setId(id);
-        training.setDatum(dtoTraining.getDatum());
-        trainingRepository.save(training);
-        return training;
-    }
-    public void deleteTraining(String id){trainingRepository.deleteById(id);}
 
-    public Training editTraining(DtoTraining dtoTraining, String id)throws Exception{
-        Training iseditTraining = trainingRepository.findById(id).orElseThrow(() ->new Exception("training nihct vorhanden"));
-        iseditTraining.setDatum(dtoTraining.getDatum());
-        return trainingRepository.save(iseditTraining);
-    }
 }
